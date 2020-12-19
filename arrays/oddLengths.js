4261d8c53c239f75abfdc3c9b92d6d53842bac3d
Hamsteez <hamza.y.c@gmail.com>
2020-12-18 22:46:01 -0500

    arrays part 12/18

---
+++ arrays/oddLengths.js
@@ -0,0 +1,8 @@
+let arr = ['a', 'abcd', 'abcde', 'abc', 'ab'];
+
+function oddLengths() {
+  let arrayLength = arr.map(len => len.length);
+  console.log(arrayLength.filter(filter => filter % 2 !== 0));
+}
+
+oddLengths();
